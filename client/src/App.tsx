50da2f486c7d08c847f2369e22bed66411f76a95
lunledev <183235731+lunledev@users.noreply.github.com>
2025-04-04 16:01:38 -0700

    added import of components needed to interact with GraphQL server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'; // components needed to interact with GraphQL server.
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
