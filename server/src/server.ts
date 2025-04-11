4a8f4d1c97577bf6c0cc8a65183c2eba9e2d00c6
lunledev <183235731+lunledev@users.noreply.github.com>
2025-04-10 21:18:26 -0700

    placed filename with dirname

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,8 +10,8 @@ import { authenticationToken } from './services/auth.js';
 import { fileURLToPath } from 'url';
 import {dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+//const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 //set up the typeDefs and resolvers for ApolloServer.
