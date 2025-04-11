f9760ef582db31b8eb4f2130ecb8963dc3a984c2
lunledev <183235731+lunledev@users.noreply.github.com>
2025-04-10 21:30:52 -0700

    revert to original

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,8 +10,8 @@ import { authenticationToken } from './services/auth.js';
 import { fileURLToPath } from 'url';
 import {dirname } from 'path';
 
-//const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 //set up the typeDefs and resolvers for ApolloServer.
